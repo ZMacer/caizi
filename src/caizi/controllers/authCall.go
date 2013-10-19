c647224857afa7941a7003fce0700589031cab05
HilerChen <hiler@cncore.(none)>
2013-10-20 00:26:41 +0800

    改用POST方法获取TOKEN

--- src/caizi/controllers/authCall.go
+++ src/caizi/controllers/authCall.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"time"
 	"strconv"
+	"net/url"
 )
 
 type AuthCallController struct {
@@ -24,7 +25,15 @@ func (this *AuthCallController) Get() {
 	strTime := strconv.Itoa(time.Now().Nanosecond())
 	ioutil.WriteFile("authCallGet"+strTime+".txt", []byte(this.Input().Encode()), 777) 
 	
-	resp, err := http.Get("https://api.weibo.com/oauth2/access_token?client_id=933700690&client_secret=efc078042a83681339b3a074ebf96854&grant_type=authorization_code&redirect_uri=http://caizi.org/auth/callback&code=" + this.Input().Get("code"))
+	data := url.Values{}
+	data.Set("client_id", "933700690")
+	data.Set("client_secret", "efc078042a83681339b3a074ebf96854")
+	data.Set("grant_type", "authorization_code")
+	data.Set("redirect_uri", "http://caizi.org/auth/callback/")
+	data.Set("code", this.Input().Get("code"))
+	
+	resp, err := http.PostForm("https://api.weibo.com/oauth2/access_token", data)
+	//resp, err := http.Get("https://api.weibo.com/oauth2/access_token?client_id=933700690&client_secret=efc078042a83681339b3a074ebf96854&grant_type=authorization_code&redirect_uri=http://caizi.org/auth/callback&code=" + this.Input().Get("code"))
 	
 	if err == nil {
 		file, _ := os.Create("token"+strTime+".txt")
