ba833d6610e4507665312741c5aae4e93ae7ad1b
HilerChen <hiler@cncore.(none)>
2013-10-20 00:11:31 +0800

    增加获取TOKEN的过程

--- src/caizi/controllers/authCall.go
+++ src/caizi/controllers/authCall.go
@@ -3,6 +3,12 @@ package controllers
 import (
 	"fmt"
 	"io/ioutil"
+	
+	"net/http"
+	"io"
+	"os"
+	"time"
+	"strconv"
 )
 
 type AuthCallController struct {
@@ -15,7 +21,17 @@ func (this *AuthCallController) Get() {
 	this.Layout = "layout.tpl"
 	this.TplNames = "index.tpl"
 	
-	ioutil.WriteFile("authCallGet.txt", []byte(this.Input().Encode()), 777) 
+	strTime := strconv.Itoa(time.Now().Nanosecond())
+	ioutil.WriteFile("authCallGet"+strTime+".txt", []byte(this.Input().Encode()), 777) 
+	
+	resp, err := http.Get("https://api.weibo.com/oauth2/access_token?client_id=933700690&client_secret=efc078042a83681339b3a074ebf96854&grant_type=authorization_code&redirect_uri=http://caizi.org/auth/callback&code=" + this.Input().Get("code"))
+	
+	if err == nil {
+		file, _ := os.Create("token"+strTime+".txt")
+		defer file.Close()
+		
+		io.Copy(file, resp.Body)
+	}
 }
 
 func (this *AuthCallController) Post() {
